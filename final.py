9e7195d1f106a48ceb9c74dff912482b30448f73
PRATYUSH RAJ <100505239+prj-07@users.noreply.github.com>
2023-07-06 13:35:27 +0530

    Update final.py

--- final.py
+++ final.py
@@ -31,24 +31,25 @@ from flask import Flask, request, jsonify
 # ]
 # '''
 
+
 json_data = '''
 [
   {
     "article_id": 1,
     "title": "what is Readyly?",
-    "content": "Readyly provides customer support AI-as-a-Service to mid-market companies. Our comprehensive solution reduces support costs by up to 10X faster than tier1 and tier 2 comapny.Our proprietary algorithms are over 90 percent accurate at data tagging and data mapping, over 6x more accurate than Zendesk at search, and can cut a customer's support costs by over 50 percent in as little as 7 days. Our founding team has a strong background in AI and customer support technology, including McKinsey and Palantir experience, as well as advisors from leading AI and customer support technology companies.",
+    "content": "Readyly provides customer support AI-as-a-Service to mid-market companies. Our comprehensive solution reduces support costs by up to 10X faster than tier1 and tier 2 comapny.Our proprietary algorithms are over 90 percent accurate at data tagging and data mapping, over 6x more accurate than Zendesk at search, and can cut a customer's support costs by over 50 percent in as little as 7 days.",
     "org_id": 12345
   },
   {
     "article_id": 2,
-    "title": "how fast is Readyly ?",
-    "content": "Our virtual agent and AI-powered help center is set up within 24 hours and provides value out of the box. In as little as 7 days, Readyly can take over your Tier 1 and Tier 2 tickets, automating them up to 100%. ",
+    "title": "How fast/quick is Readyly?",
+    "content": "Our virtual agent and AI-powered help center is set up within 24 hours and provides value out of the box. In as little as 7 days, Readyly can take over your Tier 1 and Tier 2 tickets, automating them up to 100%. they provide 10 times faster service than tier 1 and tier 2 company",
     "org_id": 67890
   },
   {
     "article_id": 3,
-    "title": "how Readyly survive competiton ?",
-    "content": "Readyly is the first platform built for fast-growth companies that builds, scales, and maintains AI and advanced automations for you, enabled by our Readyly technology suite. Companies will spend up to 98 percent of their customer support budget on labor.  In-house teams are incredibly expensive and outsourcing agencies use limited technology (or none at all) to manage their resources, leading to high overhead costs. They pass these added costs onto you, the customer.One of the biggest sources of waste is manual workflows, typically 50 percent + of all the customer support labor spend, which require agents to take multiple steps, often logging into one or more systems, in order to accomplish a task for customers. The best way to reduce or even eliminate this waste is to build advanced automations that replace these manual steps.",
+    "title": "How Readyly survive competiton in market?",
+    "content": "Readyly is the first platform built for fast-growth companies that builds, scales, and maintains AI and advanced automations for you, enabled by our Readyly technology suite. Companies will spend up to 98 percent of their customer support budget on labor.  In-house teams are incredibly expensive and outsourcing agencies use limited technology (or none at all) to manage their resources, leading to high overhead costs. .",
     "org_id": 54321
   }
   ,
@@ -57,6 +58,13 @@ json_data = '''
     "title": "what are the benefits of using  Readyly ?",
     "content": "An AI-powered help center that responds to customers with 6x the accuracy of leading ticketing solutions. An integrated NPS-based auto-survey solution captures customer voice in real-time and eliminates the need for data reconciliation from third-party survey systems.A robust analytics module that identifies and removes customer pain points.",
     "org_id": 54328
+  }
+  ,
+  {
+    "article_id": 5,
+    "title": "How accurate is Readyly algorithm ?",
+    "content": "Our comprehensive solution reduces support costs by up to 10X faster than tier1 and tier 2 comapny.Our proprietary algorithms are over 90 percent accurate at data tagging and data mapping, over 6x more accurate than Zendesk at search, and can cut a customer's support costs by over 50 percent in as little as 7 days.",
+    "org_id": 54324
   }
 
 ]
